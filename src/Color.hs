9e9de3d13bd05c33a2203cdb4e532cc3a124f3ee
PeterGriffinSr <codezz-ops@protonmail.com>
2025-04-24 12:00:03 -0400

    Added colors

---
+++ src/Color.hs
@@ -0,0 +1,10 @@
+module Color where
+
+red :: String
+red = "\x1b[31m"
+
+blue :: String
+blue = "\x1b[34m"
+
+reset :: String
+reset = "\x1b[0m"
--- src/Error.hs
+++ src/Error.hs
@@ -1,15 +1,17 @@
 module Error where
 
+import Color
+
 prettyError :: String -> String -> Int -> Int -> String -> String
 prettyError filePath sourceCode line col errMsg =
   let codeLines = lines sourceCode
       badLine = if line <= length codeLines then codeLines !! (line - 1) else ""
       caretLine = replicate (col - 1) ' ' ++ "^"
    in unlines
-        [ "error: " ++ errMsg,
-          "    --> " ++ filePath ++ ":" ++ show line ++ ":" ++ show col,
-          "    |",
-          "    |  " ++ badLine,
-          "    |  " ++ caretLine,
-          "    |"
+        [ red ++ "error: " ++ reset ++ errMsg,
+          blue ++ "    --> " ++ reset ++ filePath ++ ":" ++ show line ++ ":" ++ show col,
+          blue ++ "    |" ++ reset, 
+          blue ++ "    |  " ++ reset ++ badLine,
+          blue ++ "    |  " ++ red ++ caretLine ++ reset,
+          blue ++ "    |" ++ reset
         ]
